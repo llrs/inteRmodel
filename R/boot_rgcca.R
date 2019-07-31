17052fabc4e4a714235ed319d0e560c7bad5a04a
llrs <lluis.revilla@gmail.com>
2019-07-31 18:05:18 +0200

    Rexplain the boot methods

--- R/boot_rgcca.R
+++ R/boot_rgcca.R
@@ -1,8 +1,10 @@
 #' Bootstrap sgcca
 #'
-#' Function to perform bootstrap on the code
-#' @param ... Arguments passed to sgcca
-#' @param nb_boot Number of bootstraps to perform
+#' Function to perform bootstrap on the samples. \code{boot_samples_sgcca}
+#' bootstrap given original data, while \code{boot_index_sgcca} given some
+#' index of samples it iterates over it.
+#' @param ... Named arguments passed to sgcca.
+#' @param nb_boot Number of bootstraps to perform.
 #' @return A list with two elements: the coefficient of each variable of the
 #' input blocks; and the AVE values, both inner, and outer
 #' @export
@@ -16,13 +18,17 @@
 #'                                   "dictator")])
 #' A <- list(X_agric, X_ind, X_polit)
 #' C <- matrix(c(0, 0, 1, 0, 0, 1, 1, 1, 0), 3, 3)
-#' out <- boot_sgcca(A = A, C = C, c1 = rep(1, 3),  nb_boot = 10)
+#' out <- boot_samples_sgcca(A = A, C = C, c1 = rep(1, 3),  nb_boot = 10)
 #' head(out$AVE)
-#' @name boot
-boot_sgcca <- function(..., nb_boot = 1000) {
+#' @rdname boot
+boot_samples_sgcca <- function(..., nb_boot = 1000) {
+
   l <- list(...)
-  shrinkage <- l$c1
   A <- l$A
+  shrinkage <- l$c1
+  if (is.null(shrinkage)) {
+    shrinkage <- rep(1, length(A))
+  }
   STAB <- vector("list", length = length(l$A))
   AVE <- matrix(NA, ncol = 2, nrow = nb_boot)
   colnames(AVE) <- c("inner", "outer")
@@ -63,4 +69,47 @@ boot_sgcca <- function(..., nb_boot = 1000) {
     )
   }
   return(list("STAB" = STAB, "AVE" = AVE))
+}
+
+#' @rdname boot
+#' @param index A list of numeric values for selecting values
+#' @export
+#' @examples
+#' boots <- 10
+#' index <- vector("list", length = boots)
+#' for (i in seq_len(boots)) {
+#'   index[[i]] <- sample(nrow(A[[1]]), replace = TRUE)
+#' }
+#' boot_i <- boot_index_sgcca(index, A = A, C = C)
+boot_index_sgcca <- function(index, ...) {
+  l <- lapply(index, base_boot, ... = ...)
+  browser()
+  AVE <- sapply(l, function(x){x$AVE})
+  STAB <- sapply(seq_along(list(...)$A), function(y){
+    do.call(rbind, lapply(l, function(x){x$STAB[[y]]}))
+  })
+  list(AVE = t(AVE), STAB = STAB)
+}
+
+
+base_boot <- function(index, ...) {
+  l <- list(...)
+  STAB <- vector("list", length = length(l$A))
+  AVE <- vector("numeric", length = 2)
+  names(AVE) <- c("inner", "outer")
+  names(STAB) <- names(l$A)
+
+  l$A <- subsetData(l$A, index)
+  l$scale <- TRUE # force to scale
+
+  try({
+    res <- do.call(sgcca, l)
+    AVE["inner"] <- res$AVE$AVE_inner
+    AVE["outer"] <- res$AVE$AVE_outer
+    for (j in seq_along(l$A)) {
+      STAB[[j]] <- res$a[[j]][, 1]
+    }
+
+  }, silent = TRUE)
+  list(AVE = AVE, STAB = STAB)
 }
