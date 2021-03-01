76d02d618e8e743fa90e23cf3f0d6c17a3dcb1da
llrs <lluis.revilla@gmail.com>
2021-03-01 14:32:26 +0100

    Fix typo on tests...

--- tests/testthat/test-variables.R
+++ tests/testthat/test-variables.R
@@ -21,7 +21,7 @@ test_that("variables_contribution works", {
 
 test_that("variables_relations works", {
   w <- variables_contribution(A, result.sgcca)
-  rel <- varibales_relations(w)
+  rel <- variables_relations(w)
   expect_is(rel, "matrix")
   expect_equal(dim(rel), c(145, 92))
 })
