40cfd5e02b7e6e6e2523d7bb327fbd0aa0b7f088
llrs <lluis.revilla@gmail.com>
2021-10-18 12:45:12 +0200

    Conditional check on newest version

--- tests/testthat/test-iterate.R
+++ tests/testthat/test-iterate.R
@@ -8,27 +8,48 @@ C <- matrix(c(0, 0, 1, 0, 0, 1, 1, 1, 0), 3, 3)
 
 
 test_that("search_model works", {
-  out <- search_model(A = A, C = C, c1 =rep(1, 3), scheme = "factorial",
-                 scale = FALSE, verbose = FALSE,
-                 ncomp = rep(1, length(A)),
-                 bias = TRUE)
+  if (new_rgcca_version()) {
+    out <- search_model(A = A, C = C, tau =rep(1, 3), scheme = "factorial", method = "rgcca", scale_block = FALSE,
+                        scale = FALSE, verbose = FALSE,
+                        ncomp = rep(1, length(A)),
+                        bias = TRUE)
+  } else {
+    out <- search_model(A = A, C = C, c1 = rep(1, 3), scheme = "factorial",
+                        scale = FALSE, verbose = FALSE,
+                        ncomp = rep(1, length(A)),
+                        bias = TRUE)
+  }
   expect_s3_class(out, "data.frame")
   expect_equal(dim(out), c(20L, 10L))
 })
 
 
 test_that("iterate_model works", {
-  out <- search_model(A = A, C = C, c1 =rep(1, 3), scheme = "factorial",
-                      scale = FALSE, verbose = FALSE,
-                      ncomp = rep(1, length(A)),
-                      bias = TRUE)
+  if (new_rgcca_version()) {
+    out <- search_model(A = A, C = C, tau =rep(1, 3), scheme = "factorial", method = "rgcca", scale_block = FALSE,
+                        scale = FALSE, verbose = FALSE,
+                        ncomp = rep(1, length(A)),
+                        bias = TRUE)
+  } else {
+    out <- search_model(A = A, C = C, c1 = rep(1, 3), scheme = "factorial",
+                        scale = FALSE, verbose = FALSE,
+                        ncomp = rep(1, length(A)),
+                        bias = TRUE)
+  }
   columns <- grep("var", colnames(out))
   model <- symm(C, out[which.max(out$AVE_inner), columns])
   # We then look for a variation of the weights of this model
-  out <- iterate_model(A = A, C = model, c1 = rep(1, 3), scheme = "factorial",
-                 scale = FALSE, verbose = FALSE,
-                 ncomp = rep(1, length(A)),
-                 bias = TRUE)
+  if (new_rgcca_version()) {
+    out <- search_model(A = A, C = model, tau =rep(1, 3), scheme = "factorial", method = "rgcca", scale_block = FALSE,
+                        scale = FALSE, verbose = FALSE,
+                        ncomp = rep(1, length(A)),
+                        bias = TRUE)
+  } else {
+    out <- search_model(A = A, C = model, c1 = rep(1, 3), scheme = "factorial",
+                        scale = FALSE, verbose = FALSE,
+                        ncomp = rep(1, length(A)),
+                        bias = TRUE)
+  }
   expect_s3_class(out, "data.frame")
   expect_true(all(out$weights == 2L))
   expect_equal(dim(out), c(100L, 10L))
