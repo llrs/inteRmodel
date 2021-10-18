7d2431e976dae658cf275b9db75f6bb98321107a
llrs <lluis.revilla@gmail.com>
2021-10-18 13:19:48 +0200

    Testing with both versions

--- tests/testthat/test-improve.R
+++ tests/testthat/test-improve.R
@@ -5,10 +5,15 @@ test_that("improve works", {
   X_polit <- as.matrix(Russett[ , c("inst", "ecks",  "death", "demostab",
                                     "dictator")])
   A <- list(X_agric, X_ind, X_polit)
-  A <- lapply(A, function(x) RGCCA::scale2(x, bias = TRUE))
+  A <- lapply(A, function(x) scale2(x, bias = TRUE))
   C <- matrix(c(0, 0, 1, 0, 0, 1, 1, 1, 0), 3, 3)
-  out <- RGCCA::rgcca(A, C, tau =rep(0, 3), scheme = "factorial",
-                      scale = FALSE, verbose = FALSE, ncomp = rep(2, length(A)))
+  if (new_rgcca_version()) {
+    out <- RGCCA::rgcca(blocks = A, connection = C, tau = rep(0, 3), scheme = "factorial",
+                        scale = FALSE, verbose = FALSE, ncomp = rep(2, length(A)))
+  } else {
+    out <- RGCCA::rgcca(A = A, C = C, tau =rep(0, 3), scheme = "factorial",
+                        scale = FALSE, verbose = FALSE, ncomp = rep(2, length(A)))
+  }
 
   out <- improve(out, c("Agric", "Ind", "Polit"))
   expect_equal(dim(out$AVE$AVE_X), c(2, 3))
@@ -16,8 +21,10 @@ test_that("improve works", {
   expect_named(out$Y, c("Agric", "Ind", "Polit"))
   expect_named(out$a, c("Agric", "Ind", "Polit"))
   expect_named(out$astar, c("Agric", "Ind", "Polit"))
-  expect_equal(colnames(out$C), c("Agric", "Ind", "Polit"))
-  expect_equal(rownames(out$C), c("Agric", "Ind", "Polit"))
+  if (!new_rgcca_version()) {
+    expect_equal(colnames(out$C), c("Agric", "Ind", "Polit"))
+    expect_equal(rownames(out$C), c("Agric", "Ind", "Polit"))
+  }
 
   expect_error(improve(out, NULL), "shouldn't be NULL")
 })
