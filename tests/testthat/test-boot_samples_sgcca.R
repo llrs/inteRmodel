b498ae71977abbfcb02a14f87d1bde4ec2497e5f
llrs <lluis.revilla@gmail.com>
2021-10-19 12:20:26 +0200

    Make self contained test

--- tests/testthat/test-boot_samples_sgcca.R
+++ tests/testthat/test-boot_samples_sgcca.R
@@ -1,12 +1,12 @@
-data("Russett", package = "RGCCA")
-X_agric <- as.matrix(Russett[, c("gini", "farm", "rent")])
-X_ind <- as.matrix(Russett[, c("gnpr", "labo")])
-X_polit <- as.matrix(Russett[ , c("inst", "ecks",  "death", "demostab",
-                                  "dictator")])
-A <- list(X_agric, X_ind, X_polit)
-C <- matrix(c(0, 0, 1, 0, 0, 1, 1, 1, 0), 3, 3)
-
 test_that("boot_samples_sgcca works", {
+  data("Russett", package = "RGCCA")
+  X_agric <- as.matrix(Russett[, c("gini", "farm", "rent")])
+  X_ind <- as.matrix(Russett[, c("gnpr", "labo")])
+  X_polit <- as.matrix(Russett[ , c("inst", "ecks",  "death", "demostab",
+                                    "dictator")])
+  A <- list(X_agric, X_ind, X_polit)
+  C <- matrix(c(0, 0, 1, 0, 0, 1, 1, 1, 0), 3, 3)
+
   out <- boot_samples_sgcca(A = A, C = C, c1 = rep(1, 3),  nb_boot = 10,
                             verbose = FALSE)
   expect_equal(dim(out$AVE), c(10L, 2L))
@@ -18,6 +18,14 @@ test_that("boot_samples_sgcca works", {
 
 
 test_that("boot_index_sgcca works", {
+  data("Russett", package = "RGCCA")
+  X_agric <- as.matrix(Russett[, c("gini", "farm", "rent")])
+  X_ind <- as.matrix(Russett[, c("gnpr", "labo")])
+  X_polit <- as.matrix(Russett[ , c("inst", "ecks",  "death", "demostab",
+                                    "dictator")])
+  A <- list(X_agric, X_ind, X_polit)
+  C <- matrix(c(0, 0, 1, 0, 0, 1, 1, 1, 0), 3, 3)
+
   boots <- 10
   index <- vector("list", length = boots)
   for (i in seq_len(boots)) {
